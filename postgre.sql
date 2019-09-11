38a57e28dbca807bb31a71170125deae2d2153d1
Philip Penrose <46907267+philippenrose@users.noreply.github.com>
2019-09-10 19:32:10 -0500

    Create file
    PostreSQL query collection

---
+++ postgre.sql
@@ -0,0 +1,8 @@
+-- Create a new table
+CREATE TABLE foo (
+  foo_name VARCHAR(30) NOT NULL,
+  foo_true BOOLEAN DEFAULT false,
+  foo_type VARCHAR(10) NOT NULL,
+  bar_name VARCHAR(30),
+  bar_amt INT
+);
