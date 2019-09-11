a9aa597d94b98d0eddd8dfa5ef10f5dbe520f8a4
Philip Penrose <46907267+philippenrose@users.noreply.github.com>
2019-09-10 19:43:55 -0500

    Update postgre.sql

--- postgre.sql
+++ postgre.sql
@@ -4,7 +4,7 @@ CREATE TABLE foo (
   foo_true BOOLEAN DEFAULT false,
   foo_type VARCHAR(10) NOT NULL,
   bar_name VARCHAR(30),
-  bar_amt INT
+  foo_age INT
 );
 
 
@@ -14,11 +14,18 @@ FROM foo;
 
 
 -- Insert data into the table
-INSERT INTO foo (foo_name, foo_true, foo_type, bar_name, bar_amt)
-VALUES ('Philip', true, 'developer', 'Penrose', 1),
+INSERT INTO foo (foo_name, foo_true, foo_type, bar_name, foo_age)
+VALUES ('Philip', true, 'developer', 'Penrose', 29),
   ('Mishka', true, 'dog', 'DS', 5); 
 
   
 -- Query only the `foo_name` field
 SELECT foo_name
 FROM foo;
+
+
+-- Filter the query to show only dogs under the age of 10
+SELECT foo_type, foo_name
+FROM foo
+WHERE foo_type = 'dog'
+AND foo_age < 10;
